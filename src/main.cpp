26d97d5eea5170c04ff31433fe0cf3275e5708fb
Alex Necakov <alexnecakov@gmail.com>
2019-11-21 16:29:38 -0500

    cleaned up main

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ Display tempDisp;
 ThermistorArray tempTherms;
 
 //Define Variables we'll be connecting to
-/*double Setpoint, Input, Output;
+double Setpoint, Input, Output;
 
 //Specify the links and initial tuning parameters
 double Kp=2, Ki=5, Kd=1;
@@ -19,24 +19,20 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 int WindowSize = 5000;
 unsigned long windowStartTime;
-*/
-
 
 void setup() {
-  //windowStartTime = millis();
+  windowStartTime = millis();
 
   //init display and termperature sensing
   tempDisp = Display();
   tempTherms = ThermistorArray();
 
-  /*
   //pid has goal of 37.0
   Setpoint = 37.0;
 
   //tell the PID to range between 0 and the full window size
   myPID.SetOutputLimits(0, WindowSize);
   myPID.SetMode(AUTOMATIC);
-  */
 
   Serial.begin(9600);
 }
@@ -47,16 +43,12 @@ void loop() {
   Serial.println(tempTherms.getCurrTemp());
 
   tempDisp.refresh(tempTherms.getCurrTemp(), tempTherms.getAvgTemp(), tempTherms.getMaxTemp(), tempTherms.getMinTemp());
-  //Input = tempTherms.getCurrTemp();
-  //myPID.Compute();
+  Input = tempTherms.getCurrTemp();
+  myPID.Compute();
 
-  /************************************************
-   * turn the output pin on/off based on pid output
-   ************************************************/
-  /*if (millis() - windowStartTime > WindowSize)
-  { //time to shift the Relay Window
+  if (millis() - windowStartTime > WindowSize){ //time to shift the Relay Window
     windowStartTime += WindowSize;
   }
   if (Output < millis() - windowStartTime) digitalWrite(RELAY_PIN, HIGH);
-  else digitalWrite(RELAY_PIN, LOW);*/
+  else digitalWrite(RELAY_PIN, LOW);
 }
